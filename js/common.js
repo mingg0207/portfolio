36547d8a6e556430bffaf87c6b2cfa50b6ec3bac
mingg0207 <122245786+mingg0207@users.noreply.github.com>
2023-02-17 15:23:17 +0900

    2023.02.17 추가
    추가 및 수정사항 업데이트

--- js/common.js
+++ js/common.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
   
   $(".header .left .hbg-btn img").click(function(){
     if($(this).hasClass("off")){
-      $(this).attr("src", "../images/point-btn-on.png");
+      $(this).attr("src", "images/point-btn-on.png");
       $(this).toggleClass("off on");
       $(".hbg-nav").addClass("on");
     } else {
-      $(this).attr("src", "../images/point-btn-off.png");
+      $(this).attr("src", "images/point-btn-off.png");
       $(this).toggleClass("off on");
       $(".hbg-nav").removeClass("on");
     }

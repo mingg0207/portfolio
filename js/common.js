7e258579005584fb8f101ccefd57f29deeee7c6b
mingg0207 <122245786+mingg0207@users.noreply.github.com>
2023-02-22 15:23:17 +0900

    2023.02.22 추가
    추가 및 수정사항 업데이트

--- js/common.js
+++ js/common.js
@@ -5,7 +5,45 @@ $(document).ready(function () {
       e.preventDefault();
     }
   })
+
+  // [다크모드]
+  $("html").attr("data-dark", false)
   
+  const dark_mode = localStorage.getItem("dark_mode");
+
+  if (dark_mode == "night") {
+    $("html").attr("data-dark", "true")
+    $(".header .right .dark-btn").addClass("night");
+
+  } else {
+    $("html").attr("data-dark", "false")
+    $(".header .right .dark-btn").removeClass("night");
+
+  }
+
+  // PC, 태블릿 다크모드 버튼
+
+  $(".header .right .dark-btn").click(function(e){
+    
+    e.preventDefault()
+
+    if ($("html").attr("data-dark") == "true") {
+      $("html").attr("data-dark", "false")
+
+      // 라이트 모드시 설정 초기화
+      localStorage.removeItem("dark_mode")
+    } else {
+      $("html").attr("data-dark", "true")
+
+      // 다크모드 유지
+      localStorage.setItem("dark_mode", "night")
+    }
+
+    $(".header .right .dark-btn").toggleClass("night");
+
+  })
+
+
   $(".header .left .hbg-btn img").click(function(){
     if($(this).hasClass("off")){
       $(this).attr("src", "images/point-btn-on.png");
